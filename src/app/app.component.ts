ca5c5a9d56851756d5d4ceed60dfb52324f54675
kei <34824796+kei-to@users.noreply.github.com>
2025-04-21 00:14:18 +0900

    actions 追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,19 +43,6 @@ export class AppComponent {
     this.elements.forEach((_, i) => this.checkIfTimeReached(i));
   }
 
-  // elementTimeCompea(index: number){
-  //   if(this.elements[index].flg){
-  //     return;
-  //   }
-  //   const [hours, minutes] = this.elements[index].text.split(':').map(Number);
-  //   let time: Date = new Date();
-  //   time.setHours(hours, minutes, 0, 0);
-  //   if( time < new Date ){
-  //     this.elements[index].className = this.elements[index].className == "normal" ? "highlight":"normal";
-  //     AppComponent.beep();
-  //   }
-  // }
-
   checkIfTimeReached(index: number): void {
     const element = this.elements[index];
     if (element.alertDisabled) return;
@@ -70,26 +57,16 @@ export class AppComponent {
     // tslint:disable-next-line:max-line-length
     const base64 = 'UklGRnoGAABXQVZFZm10IBAAAAABAAEAQB8AAEAfAAABAAgAZGF0YQoGAACBhYqFbF1fdJivrJBhNjVgodDbq2EcBj+a2/LDciUFLIHO8tiJNwgZaLvt559NEAxQp+PwtmMcBjiR1/LMeSwFJHfH8N2QQAoUXrTp66hVFApGn+DyvmwhBTGH0fPTgjMGHm7A7+OZSA0PVqzn77BdGAg+ltryxnMpBSl+zPLaizsIGGS57OihUBELTKXh8bllHgU2jdXzzn0vBSF1xe/glEILElyx6OyrWBUIQ5zd8sFuJAUuhM/z1YU2Bhxqvu7mnEoODlOq5O+zYBoGPJPY88p2KwUme8rx3I4+CRZiturqpVITC0mi4PK8aB8GM4nU8tGAMQYfcsLu45ZFDBFYr+ftrVoXCECY3PLEcSYELIHO8diJOQcZaLvt559NEAxPqOPwtmMcBjiP1/PMeS0GI3fH8N2RQAoUXrTp66hVFApGnt/yvmwhBTCG0fPTgjQGHW/A7eSaRw0PVqzl77BeGQc9ltvyxnUoBSh+zPDaizsIGGS56+mjTxELTKXh8bllHgU1jdT0z3wvBSJ0xe/glEILElyx6OyrWRUIRJve8sFuJAUug8/y1oU2Bhxqvu3mnEoPDlOq5O+zYRsGPJLZ88p3KgUme8rx3I4+CRVht+rqpVMSC0mh4fK8aiAFM4nU8tGAMQYfccPu45ZFDBFYr+ftrVwWCECY3PLEcSYGK4DN8tiIOQcZZ7zs56BODwxPpuPxtmQcBjiP1/PMeywGI3fH8N+RQAoUXrTp66hWEwlGnt/yv2wiBDCG0fPTgzQHHG/A7eSaSQ0PVqvm77BeGQc9ltrzxnUoBSh9y/HajDsIF2W56+mjUREKTKPi8blnHgU1jdTy0HwvBSF0xPDglEQKElux6eyrWRUJQ5vd88FwJAQug8/y1oY2Bhxqvu3mnEwODVKp5e+zYRsGOpPX88p3KgUmecnw3Y4/CBVhtuvqpVMSC0mh4PG9aiAFM4nS89GAMQYfccLv45dGCxFYrufur1sYB0CY3PLEcycFKoDN8tiIOQcZZ7rs56BODwxPpuPxtmQdBTiP1/PMey4FI3bH8d+RQQkUXbPq66hWFQlGnt/yv2wiBDCG0PPTgzUGHG3A7uSaSQ0PVKzm7rJeGAc9ltrzyHQpBSh9y/HajDwIF2S46+mjUREKTKPi8blnHwU1jdTy0H4wBiF0xPDglEQKElux5+2sWBUJQ5vd88NvJAUtg87y1oY3Bxtpve3mnUsODlKp5PC1YRsHOpHY88p3LAUlecnw3Y8+CBZhtuvqpVMSC0mh4PG9aiAFMojT89GBMgUfccLv45dGDRBYrufur1sYB0CX2/PEcycFKoDN8tiKOQgZZ7vs56BOEQxPpuPxt2MdBTeP1vTNei4FI3bH79+RQQsUXbTo7KlXFAlFnd7zv2wiBDCF0fLUgzUGHG3A7uSaSQ0PVKzm7rJfGQc9lNrzyHUpBCh9y/HajDwJFmS46+mjUhEKTKLh8btmHwU1i9Xyz34wBiFzxfDglUMMEVux5+2sWhYIQprd88NvJAUsgs/y1oY3Bxpqve3mnUsODlKp5PC1YhsGOpHY88p5KwUlecnw3Y8+ChVgtunqp1QTCkig4PG9ayEEMojT89GBMgUfb8Lv4pdGDRBXr+fur1wXB0CX2/PEcycFKn/M8diKOQgZZrvs56BPEAxOpePxt2UcBzaP1vLOfC0FJHbH79+RQQsUXbTo7KlXFAlFnd7xwG4jBS+F0fLUhDQGHG3A7uSbSg0PVKrl7rJfGQc9lNn0yHUpBCh7yvLajTsJFmS46umkUREMSqPh8btoHgY0i9Tz0H4wBiFzw+/hlUULEVqw6O2sWhYIQprc88NxJQUsgs/y1oY3BxpqvO7mnUwPDVKo5PC1YhsGOpHY8sp5KwUleMjx3Y9ACRVgterqp1QTCkig3/K+aiEGMYjS89GBMgceb8Hu45lHDBBXrebvr1wYBz+Y2/PGcigEKn/M8dqJOwgZZrrs6KFOEAxOpd/js2coGUCLydq6e0MlP3uwybiNWDhEa5yztJRrS0lnjKOkk3leWGeAlZePfHRpbH2JhoJ+fXl9TElTVEQAAABJTkZPSUNSRAsAAAAyMDAxLTAxLTIzAABJRU5HCwAAAFRlZCBCcm9va3MAAElTRlQQAAAAU291bmQgRm9yZ2UgNC41AA==';
     const sound = new Audio('data:audio/wav;base64,' + base64);
-    sound.volume = 0.05;
+    sound.volume = 0.02;
     sound.play();
   }
-
-  // addElement() {
-  //   const index = this.elements.length + 1;
-  //   this.elements.push({ 
-  //     text: '00:00', 
-  //     selectedOption: this.dropdownOptions[0], 
-  //     className: 'normal',
-  //     flg: false,
-  //   });
-  // }
 
   addElement() {
     const index = this.elements.length + 1;
     const now = new Date();
     const newElement: TimeElementModel = {
-      label: `テキスト ${index}`,
-      time: new Date(now.setHours(0, 0, 0, 0)), // 初期 00:00
+      label: '00:00',
+      time: new Date(now.setHours(0, 0, 0, 0)),
       selectedMinutes: null,
       className: 'normal',
       alertDisabled: true,
@@ -111,22 +88,20 @@ export class AppComponent {
     const element = this.elements[index];
     if (element.selectedMinutes != null) {
       const future = new Date();
-      console.log(`future:`, future);
       future.setMinutes(future.getMinutes() + Number(element.selectedMinutes));
       element.time = future;
-      console.log(`future:`, future);
-      console.log(`element.selectedMinutes:`, element.selectedMinutes);
-      element.selectedMinutes
-      this.updateTima(index, future )
-      this.updateClassName(index, 'highlight');
+      element.selectedMinutes;
+      element.label = this.formatTime(future);
+      this.updateTima(index, future );
+      this.updateClassName(index, 'normal');
     }
   }
 
-  updateTima(index: number, time :Date ){
+  updateTima(index: number, time: Date ){
     this.elements[index].time =time;
   }
 
-  isValidTimeString(time: string):boolean{
+  isValidTimeString(time: string): boolean{
     const timeRegex = /^([01]\d|2[0-3]):([0-5]\d)$/;
     return timeRegex.test(time);
   }
