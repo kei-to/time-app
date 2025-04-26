40d301b4f8194a543a758a9d6a5e35ea47ecf408
kei <34824796+kei-to@users.noreply.github.com>
2025-04-26 16:27:48 +0900

    android/ios プロジェクト追加

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'time-app',
+  webDir: 'dist/time-app'
+};
+
+export default config;
